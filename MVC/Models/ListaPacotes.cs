904a54e6f1f54edbe3dbc71372189df0d5443577
vitaoTM <vitor.grosskopf@gmail.com>
2022-10-22 09:18:27 -0300

    criando classe

--- MVC/Models/ListaPacotes.cs
+++ MVC/Models/ListaPacotes.cs
@@ -8,20 +8,15 @@ namespace MVC.Models
     {
        List<Pacotes> ListaPacotes = new List<Pacotes>();
 
-       public void Adicionar(Pacotes pacoteNovo)
-       {
+        public void Adicionar(Pacotes pacoteNovo)
+        {
             ListaPacotes.Add(pacoteNovo);
-       } 
-       public Mostrar()
-       {
-            for(i = 0; i < listaPacotes.Count; i++)
-            {
-
-                Console.WriteLine("Pacotes Cadastrados");
-                Console.WriteLine($"Pacote {listaPacotes[].NomePacote}");
-                Console.WriteLine($"Valor: {listaPacotes[].Valor}");
-                Console.WriteLine($"Destino: {listaPacotes[].Destino}");
-            }
-       }
+        } 
+        public static List<ClasseAgendar> Total()
+    
+        {
+            return dados;
+        }
+       
     }
 }
